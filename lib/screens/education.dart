1b6a21d053dcd2286ed7ec4be3440dfb3b39a9b7
Dhia Eddine Nini <dxn00@mail.aub.edu>
2023-01-15 20:53:08 +0200

    Added Educational Content page

---
+++ lib/screens/education.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:said/theme/text_styles.dart';
+import 'package:said/widgets/said_text_field.dart';
+
+class EducationPage extends StatelessWidget {
+  const EducationPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(
+        child:
+          Column(
+            children: [
+              Text(AppLocalizations.of(context).educationContent,
+              style: subHeader()),
+              SaidTextField(
+                placeholder: AppLocalizations.of(context).search,
+              ),
+              
+            ],
+          )
+      )
+    )
+  }
+}
