b4f8c5ef1a47e9fbdd80dabf1d41618cb62b3f02
Dhia Eddine Nini <dxn00@mail.aub.edu>
2023-01-17 23:35:13 +0200

    Added expansion panels content to the education page

--- lib/screens/education.dart
+++ lib/screens/education.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:said/theme/text_styles.dart';
-import 'package:said/widgets/said_bottom_nav_bar.dart';
 import 'package:said/widgets/said_text_field.dart';
 
 class EducationPage extends StatefulWidget {
@@ -12,10 +11,176 @@ class EducationPage extends StatefulWidget {
 }
 
 class _EducationPageState extends State<EducationPage> {
-  final List<bool> _isOpen = List.generate(2, (index) => false);
+  final List<bool> _isOpen = List.generate(20, (index) => false);
+
+  List<ExpansionPanel> generateExpansionPanels(
+      List<String> titles, List<Widget> bodies) {
+    if (titles.length != bodies.length) {
+      throw Exception(
+          "The List of Titles and Bodies must be of the same length.");
+    }
+
+    return List<ExpansionPanel>.generate(
+        titles.length,
+        (i) => ExpansionPanel(
+            canTapOnHeader: true,
+            isExpanded: _isOpen[0],
+            headerBuilder: (context, isOpen) {
+              return Padding(
+                  padding: const EdgeInsets.all(16), child: Text(titles[i]));
+            },
+            body:
+                Padding(padding: const EdgeInsets.all(16), child: bodies[i])));
+  }
 
   @override
   Widget build(BuildContext context) {
+    // prepare titles:
+    final List<String> titles = [
+      AppLocalizations.of(context).whatIsCrcTitle,
+      AppLocalizations.of(context).symptomsOfCrcTitle,
+      AppLocalizations.of(context).riskTitle,
+      AppLocalizations.of(context).screeningTypesTitle,
+      AppLocalizations.of(context).stagesOfCrcTitle,
+      AppLocalizations.of(context).screeningImportance,
+      AppLocalizations.of(context).screeningGuidelines,
+      AppLocalizations.of(context).preventionTitle,
+      AppLocalizations.of(context).stats,
+      AppLocalizations.of(context).communitySupportTitle,
+    ];
+
+    // prepare bodies:
+    final List<Widget> bodies = [
+      Column(
+        children: [
+          Text(AppLocalizations.of(context).whatIsCrcBody1),
+          Text(AppLocalizations.of(context).whatIsCrcBody2),
+        ],
+      ),
+      Column(
+        children: [
+          Text(AppLocalizations.of(context).symptom1),
+          Text(AppLocalizations.of(context).symptom2),
+          Text(AppLocalizations.of(context).symptom3),
+          Text(AppLocalizations.of(context).symptom4),
+          Text(AppLocalizations.of(context).symptom5),
+          Text(AppLocalizations.of(context).symptom6),
+        ],
+      ),
+      Column(
+        children: [
+          Text(AppLocalizations.of(context).riskBody),
+          Text(AppLocalizations.of(context).risk1),
+          Text(AppLocalizations.of(context).risk2),
+          Text(AppLocalizations.of(context).risk2a),
+          Text(AppLocalizations.of(context).risk2b),
+          Text(AppLocalizations.of(context).risk3),
+          Text(AppLocalizations.of(context).risk4),
+          Text(AppLocalizations.of(context).risk5),
+          Text(AppLocalizations.of(context).risk6),
+        ],
+      ),
+      Column(
+        children: [
+          Text(AppLocalizations.of(context).screeningTypes),
+          Text(AppLocalizations.of(context).screeningType1Title),
+          Text(AppLocalizations.of(context).screeningType1Body),
+          Text(AppLocalizations.of(context).screeningType2Title),
+          Text(AppLocalizations.of(context).screeningType2Body),
+        ],
+      ),
+      Column(
+        children: [
+          Text(AppLocalizations.of(context).screeningImportance),
+          Text(AppLocalizations.of(context).screeningMsg1),
+          Text(AppLocalizations.of(context).screeningMsg2),
+          Text(AppLocalizations.of(context).screeningMsg3),
+          Text(AppLocalizations.of(context).screeningMsg4),
+          Text(AppLocalizations.of(context).screeningMsg5),
+        ],
+      ),
+      Column(
+        children: [
+          Text(AppLocalizations.of(context).screeningGuidelines),
+          Text(AppLocalizations.of(context).screeningGHighRisk),
+          Text(AppLocalizations.of(context).screeningGHighRisk1),
+          Text(AppLocalizations.of(context).screeningGHighRisk2),
+          Text(AppLocalizations.of(context).screeningGHighRisk3),
+          Text(AppLocalizations.of(context).screeningGHighRisk4),
+          Text(AppLocalizations.of(context).screeningGLowRisk),
+          Text(AppLocalizations.of(context).screeningGLowRisk1),
+        ],
+      ),
+      Column(
+        children: [
+          Text(AppLocalizations.of(context).preventionQ),
+          Text(AppLocalizations.of(context).preventionA),
+          Text(AppLocalizations.of(context).preventionTitle),
+          Text(AppLocalizations.of(context).screeningGHighRisk2),
+          Text(AppLocalizations.of(context).screeningGHighRisk3),
+          Text(AppLocalizations.of(context).screeningGHighRisk4),
+          Text(AppLocalizations.of(context).screeningGLowRisk),
+          Text(AppLocalizations.of(context).screeningGLowRisk1),
+          ExpansionPanelList(
+            animationDuration: const Duration(seconds: 1),
+            children: [
+              ExpansionPanel(
+                  canTapOnHeader: true,
+                  isExpanded: _isOpen[0],
+                  headerBuilder: (context, isOpen) {
+                    return Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Text(
+                            AppLocalizations.of(context).primaryPrevention));
+                  },
+                  body: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Text(
+                          AppLocalizations.of(context).primaryPreventionBody))),
+              ExpansionPanel(
+                  canTapOnHeader: true,
+                  isExpanded: _isOpen[0],
+                  headerBuilder: (context, isOpen) {
+                    return Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Text(
+                            AppLocalizations.of(context).tertiaryPrevention));
+                  },
+                  body: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Text(
+                          AppLocalizations.of(context).tertiaryPreventionBody)))
+            ],
+          )
+        ],
+      ),
+      Column(
+        children: [
+          Text(AppLocalizations.of(context).stat1),
+          Text(AppLocalizations.of(context).stat2),
+          Text(AppLocalizations.of(context).stat3),
+          Text(AppLocalizations.of(context).stat4),
+          Text(AppLocalizations.of(context).stat5),
+        ],
+      ),
+      Column(
+        children: [
+          Text(AppLocalizations.of(context).communityM1),
+          Text(AppLocalizations.of(context).communityM2title),
+          Text(AppLocalizations.of(context).communityM2pt1),
+          Text(AppLocalizations.of(context).communityM2pt2),
+          Text(AppLocalizations.of(context).communityM2pt3),
+          Text(AppLocalizations.of(context).communityM2pt4),
+          Text(AppLocalizations.of(context).communityM2pt5),
+          Text(AppLocalizations.of(context).communityM2pt6),
+          Text(AppLocalizations.of(context).communityM2pt7),
+          Text(AppLocalizations.of(context).communityM3),
+        ],
+      ),
+    ];
+
+    List<ExpansionPanel> panels = generateExpansionPanels(titles, bodies);
+
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.transparent,
@@ -37,65 +202,7 @@ class _EducationPageState extends State<EducationPage> {
                   const Padding(padding: EdgeInsets.all(8)),
                   ExpansionPanelList(
                     animationDuration: const Duration(seconds: 1),
-                    children: [
-                      ExpansionPanel(
-                          canTapOnHeader: true,
-                          isExpanded: _isOpen[0],
-                          headerBuilder: (context, isOpen) {
-                            return const Padding(
-                                padding: EdgeInsets.all(16),
-                                child: Text("Header"));
-                          },
-                          body: const Padding(
-                              padding: EdgeInsets.all(16),
-                              child: Text("Inside Expandable"))),
-                      ExpansionPanel(
-                          canTapOnHeader: true,
-                          isExpanded: _isOpen[1],
-                          headerBuilder: (context, isOpen) {
-                            return const Padding(
-                                padding: EdgeInsets.all(16),
-                                child: Text("Header"));
-                          },
-                          body: const Text("Inside Expandable")),
-                      ExpansionPanel(
-                          canTapOnHeader: true,
-                          isExpanded: _isOpen[1],
-                          headerBuilder: (context, isOpen) {
-                            return const Padding(
-                                padding: EdgeInsets.all(16),
-                                child: Text("Header"));
-                          },
-                          body: const Text("Inside Expandable")),
-                      ExpansionPanel(
-                          canTapOnHeader: true,
-                          isExpanded: _isOpen[1],
-                          headerBuilder: (context, isOpen) {
-                            return const Padding(
-                                padding: EdgeInsets.all(16),
-                                child: Text("Header"));
-                          },
-                          body: const Text("Inside Expandable")),
-                      ExpansionPanel(
-                          canTapOnHeader: true,
-                          isExpanded: _isOpen[1],
-                          headerBuilder: (context, isOpen) {
-                            return const Padding(
-                                padding: EdgeInsets.all(16),
-                                child: Text("Header"));
-                          },
-                          body: const Text("Inside Expandable")),
-                      ExpansionPanel(
-                          canTapOnHeader: true,
-                          isExpanded: _isOpen[1],
-                          headerBuilder: (context, isOpen) {
-                            return const Padding(
-                                padding: EdgeInsets.all(16),
-                                child: Text("Header"));
-                          },
-                          body: const Text("Inside Expandable"))
-
-                    ],
+                    children: panels,
                     expansionCallback: (i, isOpen) {
                       setState(() {
                         _isOpen[i] = !isOpen;
