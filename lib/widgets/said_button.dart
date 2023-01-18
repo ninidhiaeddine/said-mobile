d6ccda2881aaa8294ec35d7e9725fe82031e2be5
Dhia Eddine Nini <dxn00@mail.aub.edu>
2023-01-18 11:40:36 +0200

    Added Share milestone UI

--- lib/widgets/said_button.dart
+++ lib/widgets/said_button.dart
@@ -5,12 +5,14 @@ class SaidButton extends StatelessWidget {
       {super.key,
       required this.text,
       required this.context,
+      this.enabled = true,
       this.icon,
       this.linkTo,
       this.onPressed});
 
   final String text;
   final BuildContext context;
+  final bool enabled;
   final Widget? icon;
   final Widget? linkTo;
   final Function? onPressed;
@@ -18,13 +20,24 @@ class SaidButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (icon != null) {
+      if (enabled) {
+        return ElevatedButton(
+            onPressed: () => {
+                  linkTo == null
+                      ? onPressed
+                      : Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => linkTo!))
+                },
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Text(text, style: const TextStyle(fontWeight: FontWeight.bold)),
+              const SizedBox(
+                width: 5,
+              ),
+              icon!,
+            ]));
+      }
       return ElevatedButton(
-          onPressed: () => {
-                linkTo == null
-                    ? onPressed
-                    : Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => linkTo!))
-              },
+          onPressed: null,
           child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
             Text(text, style: const TextStyle(fontWeight: FontWeight.bold)),
             const SizedBox(
@@ -33,13 +46,23 @@ class SaidButton extends StatelessWidget {
             icon!,
           ]));
     } else {
+      if (enabled) {
+        return ElevatedButton(
+            onPressed: () => {
+                  linkTo == null
+                      ? onPressed
+                      : Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => linkTo!))
+                },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(text, style: const TextStyle(fontWeight: FontWeight.bold))
+              ],
+            ));
+      }
       return ElevatedButton(
-          onPressed: () => {
-                linkTo == null
-                    ? onPressed
-                    : Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => linkTo!))
-              },
+          onPressed: null,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
--- lib/widgets/said_steps_counter.dart
+++ lib/widgets/said_steps_counter.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:said/config/color_constants.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:said/widgets/said_button.dart';
 
 class SaidStepsCounter extends StatefulWidget {
   const SaidStepsCounter(
@@ -14,6 +15,8 @@ class SaidStepsCounter extends StatefulWidget {
 }
 
 class _SaidStepsCounterState extends State<SaidStepsCounter> {
+  final bool goalReached = false;
+
   double _computeProgressValue() {
     return widget.stepsDone / widget.stepsGoal;
   }
@@ -22,21 +25,23 @@ class _SaidStepsCounterState extends State<SaidStepsCounter> {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 300,
+      height: 350,
       decoration: const BoxDecoration(
           color: ColorConstants.secondaryColor,
           borderRadius: BorderRadius.all(Radius.circular(16))),
       child: Stack(alignment: Alignment.center, children: [
-        SizedBox(
-          width: 200,
-          height: 200,
-          child: CircularProgressIndicator(
-            value: _computeProgressValue(),
-            strokeWidth: 20,
-            color: ColorConstants.primaryColor,
-            backgroundColor: const Color(0xff2d4385),
-          ),
-        ),
+        Positioned(
+            top: 50,
+            child: SizedBox(
+              width: 200,
+              height: 200,
+              child: CircularProgressIndicator(
+                value: _computeProgressValue(),
+                strokeWidth: 20,
+                color: ColorConstants.primaryColor,
+                backgroundColor: const Color(0xff2d4385),
+              ),
+            )),
         Positioned(
             top: 110,
             child: Text(AppLocalizations.of(context).steps,
@@ -58,12 +63,19 @@ class _SaidStepsCounterState extends State<SaidStepsCounter> {
                   style: const TextStyle(color: Colors.white, fontSize: 18),
                 ),
                 Text(widget.stepsGoal.toString(),
-                    style: TextStyle(
+                    style: const TextStyle(
                         color: ColorConstants.primaryColor,
                         fontWeight: FontWeight.bold,
                         fontSize: 18)),
               ],
-            ))
+            )),
+        Positioned(
+            bottom: 16,
+            child: SaidButton(
+                text: AppLocalizations.of(context).shareMilestone,
+                context: context,
+                icon: const Icon(Icons.star_purple500),
+                enabled: goalReached))
       ]),
     );
   }
