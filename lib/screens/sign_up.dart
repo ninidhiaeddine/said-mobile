40f1706bbfa1185f265cb4ca327a703e8f773ba1
Dhia Eddine Nini <dxn00@mail.aub.edu>
2023-01-19 01:36:24 +0200

    Stored state of fields and made API call to register

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:said/config/api_constants.dart';
 import 'package:said/screens/user_navigator_parent.dart';
+import 'package:said/services/dtos/register.dart';
+import 'package:said/services/register_service.dart';
 import 'package:said/theme/text_styles.dart';
 import 'package:said/widgets/said_button.dart';
 import 'package:said/widgets/said_password_field.dart';
@@ -14,6 +17,17 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  String _usernameValue = "";
+  String _emailValue = "";
+  String _passwordValue = "";
+
+  Future<void> signUp() async {
+    var registerDto = Register(
+        username: _usernameValue, email: _emailValue, password: _passwordValue);
+    var response = await RegisterService.register(registerDto);
+    print(response.body);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,20 +47,37 @@ class _SignUpPageState extends State<SignUpPage> {
               SaidTextField(
                 placeholder: AppLocalizations.of(context).username,
                 textInputType: TextInputType.text,
+                callback: (newValue) => {
+                  setState(() {
+                    _usernameValue = newValue;
+                  })
+                },
               ),
               const Padding(padding: EdgeInsets.all(8)),
               SaidTextField(
                 placeholder: AppLocalizations.of(context).email,
                 textInputType: TextInputType.emailAddress,
-                suffixIcon: Icon(Icons.email_rounded),
+                suffixIcon: const Icon(Icons.email_rounded),
+                callback: (newValue) => {
+                  setState(() {
+                    _emailValue = newValue;
+                  })
+                },
               ),
               const Padding(padding: EdgeInsets.all(8)),
-              const SaidPasswordField(),
+              SaidPasswordField(
+                callback: (newValue) => {
+                  setState(() {
+                    _passwordValue = newValue;
+                  })
+                },
+              ),
               const Padding(padding: EdgeInsets.all(12)),
               SaidButton(
-                  text: AppLocalizations.of(context).signUp,
-                  context: context,
-                  linkTo: const UserNavigatorParent()),
+                text: AppLocalizations.of(context).signUp,
+                context: context,
+                onPressed: signUp,
+              ),
               const Padding(padding: EdgeInsets.all(24)),
             ]),
       ),
