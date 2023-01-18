f8bfb19d9da5853cc00461f5f583da7d9b6cdfdb
Dhia Eddine Nini <dxn00@mail.aub.edu>
2023-01-18 17:05:58 +0200

    Put all API constants in one file

---
+++ lib/config/api_constants.dart
@@ -0,0 +1,15 @@
+class ApiConstants {
+  static String baseUrl = 'http://localhost:1337/api';
+
+  // endpoints:
+  static String usersEndpoint = '/users';
+  static String registerEndpoint = '/auth/local/register';
+  static String authEndpoint = '/auth/local';
+  static String announcementsEndpoint = '/announcements';
+  static String medicationsEndpoint = '/medications';
+  static String medicationRemindersEndpoint = '/medication-reminders';
+  static String messagesEndpoint = '/messages';
+  static String postsEndpoint = '/posts';
+  static String postLikesEndpoint = '/post-likes';
+  static String stepCountersEndpoint = '/step-counters';
+}
