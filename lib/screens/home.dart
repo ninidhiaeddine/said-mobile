cd0041dfaeeaf9e6154a7f20a8cc2c633754702e
Dhia Eddine Nini <dxn00@mail.aub.edu>
2023-01-19 21:54:18 +0200

    Made Home page check whether user is logged in or not before proceeding

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_session_manager/flutter_session_manager.dart';
 import 'package:said/screens/sign_in.dart';
+import 'package:said/screens/user_navigator_parent.dart';
+import 'package:said/utils/navigator.dart';
+import 'package:said/utils/said_session_manager.dart';
 import 'package:said/widgets/said_button.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  Future<void> _checkLoggedInStatus() async {
+    // check whether user is logged in:
+    if (await SaidSessionManager.containsKey("isLoggedIn") &&
+        await SaidSessionManager.getSessionValue("isLoggedIn")) {
+      navigateToRoute(context, const UserNavigatorParent());
+    }
+  }
+
+  @override
+  initState() {
+    super.initState();
+    _checkLoggedInStatus();
+  }
 
   @override
   Widget build(BuildContext context) {
