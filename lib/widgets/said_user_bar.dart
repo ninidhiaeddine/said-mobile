1dbb0e8dfc448a7c9fc87ffb024fd9ec95e42469
Dhia Eddine Nini <dxn00@mail.aub.edu>
2023-01-19 21:56:07 +0200

    Added a name initials extractor (based off user's full name)

--- lib/widgets/said_user_bar.dart
+++ lib/widgets/said_user_bar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:said/theme/text_styles.dart';
-import 'package:said/widgets/said_profile_pic.dart';
+import 'package:said/widgets/said_avatar.dart';
 
 class SaidUserBar extends StatefulWidget {
   const SaidUserBar({super.key, this.userFullName = "John Doe", this.meds = 0});
@@ -14,13 +14,28 @@ class SaidUserBar extends StatefulWidget {
 }
 
 class _SaidUserBarState extends State<SaidUserBar> {
+  String _extractNameInitials(String userFullName) {
+    String nameInitials = "";
+
+    var names = userFullName.toUpperCase().split(' ');
+    for (var value in names) {
+      if (nameInitials.length >= 2) {
+        break;
+      }
+
+      nameInitials += value[0];
+    }
+
+    return nameInitials;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(32.0, 16.0, 32.0, 16.0),
       child: Column(
         children: [
-          const SaidProfilePic(),
+          SaidAvatar(nameInitials: _extractNameInitials(widget.userFullName)),
           const Padding(padding: EdgeInsets.all(12.0)),
           Row(
             children: [
