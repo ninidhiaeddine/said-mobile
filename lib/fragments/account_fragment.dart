858047ca4830eb997fa3adb13141beeb91e9b001
Dhia Eddine Nini <dxn00@mail.aub.edu>
2023-03-21 21:50:48 +0200

    Fixed padding in Account Fragment and made it expect authenticatedUser and callback onRefreshUser

--- lib/fragments/account_fragment.dart
+++ lib/fragments/account_fragment.dart
@@ -12,10 +12,12 @@ import 'package:said/widgets/misc/said_user_bar.dart';
 import 'package:said/widgets/textfields/said_text_field.dart';
 
 class AccountFragment extends StatefulWidget {
-  const AccountFragment({Key? key, required this.authenticatedUser})
+  const AccountFragment(
+      {Key? key, required this.authenticatedUser, required this.onRefreshUser})
       : super(key: key);
 
   final User authenticatedUser;
+  final Function(User) onRefreshUser;
 
   @override
   State<AccountFragment> createState() => _AccountFragmentState();
@@ -25,17 +27,6 @@ class _AccountFragmentState extends State<AccountFragment> {
   String _sexValue = "";
   final controllers = List.generate(3, (index) => TextEditingController());
   late List<String> sexOptions;
-
-  Future<void> _saveUserToSessionAsync(User updatedUser) async {
-    SaidSessionManager.storeUser(
-        id: updatedUser.id!,
-        username: updatedUser.username,
-        email: updatedUser.email,
-        firstName: updatedUser.firstName,
-        lastName: updatedUser.lastName,
-        sex: updatedUser.sex,
-        age: updatedUser.age);
-  }
 
   Future<void> _saveChangesAsync() async {
     User oldUser = await SaidSessionManager.getUser();
@@ -50,20 +41,26 @@ class _AccountFragmentState extends State<AccountFragment> {
             : null,
         sex: _sexValue.isNotEmpty ? _sexValue : null);
 
-    // 1. make api call:
+    // 1. make api call to update user:
     var response = await UserService.updateUser(updatedUser.id!, updatedUser);
 
     if (response.statusCode == 200) {
       // save user to local session storage:
-      _saveUserToSessionAsync(updatedUser);
+      //_saveUserToSessionAsync(updatedUser);
 
+      // check if context is ready to be used:
       if (!mounted) {
         return;
       }
 
+      // callback to reload user in user screen:
+      widget.onRefreshUser(updatedUser);
+
       // show snackbar:
       final snackBar = SnackBar(
-        content: Text(AppLocalizations.of(context).changesSavedSuccess),
+        content: Text(AppLocalizations
+            .of(context)
+            .changesSavedSuccess),
         backgroundColor: Colors.green,
         behavior: SnackBarBehavior.floating,
       );
@@ -79,7 +76,9 @@ class _AccountFragmentState extends State<AccountFragment> {
       // show snackbar:
       final snackBar = SnackBar(
         content:
-            Text('${AppLocalizations.of(context).changesSavedError}: $errMsg'),
+        Text('${AppLocalizations
+            .of(context)
+            .changesSavedError}: $errMsg'),
         backgroundColor: Colors.red,
         behavior: SnackBarBehavior.floating,
       );
@@ -90,102 +89,118 @@ class _AccountFragmentState extends State<AccountFragment> {
   @override
   void initState() {
     super.initState();
-    sexOptions = [
-      AppLocalizations.of(context).male,
-      AppLocalizations.of(context).female
-    ];
   }
 
   @override
   Widget build(BuildContext context) {
+    sexOptions = [
+      AppLocalizations
+          .of(context)
+          .male,
+      AppLocalizations
+          .of(context)
+          .female
+    ];
     return Scaffold(
-        body: SingleChildScrollView(
+      body: SingleChildScrollView(
+        child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32),
             child: Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 32),
-          child: Builder(builder: (context) {
-            if (widget.authenticatedUser.firstName != null) {
-              var fullName =
-                  '${widget.authenticatedUser.firstName} ${widget.authenticatedUser.lastName}';
-              return SaidUserBar(
-                userFullName: fullName,
-              );
-            } else {
-              return SaidUserBar(
-                userFullName: widget.authenticatedUser.username,
-              );
-            }
-          }),
+                children: [
+                Builder(builder: (context) {
+        if (widget.authenticatedUser.firstName != null) {
+        var fullName =
+        '${widget.authenticatedUser.firstName} ${widget.authenticatedUser.lastName}';
+        return SaidUserBar(
+        userFullName: fullName,
+        );
+        } else {
+        return SaidUserBar(
+        userFullName: widget.authenticatedUser.username,
+        );
+        }
+        }),
+        const SizedBox(
+          height: 32,
         ),
-        const Padding(padding: EdgeInsets.all(16)),
         Text(
-          AppLocalizations.of(context).accountSettings,
+          AppLocalizations
+              .of(context)
+              .accountSettings,
           style: subHeader(),
         ),
-        Padding(
-            padding: const EdgeInsets.all(48.0),
-            child: Column(
-              children: [
-                SaidTextField(
-                  placeholder: AppLocalizations.of(context).firstName,
-                  text: widget.authenticatedUser.firstName,
-                  textInputType: TextInputType.name,
-                  callback: (newValue, controller) {
-                    setState(() {
-                      controllers[0] = controller;
-                    });
-                  },
-                ),
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                ),
-                SaidTextField(
-                  placeholder: AppLocalizations.of(context).lastName,
-                  text: widget.authenticatedUser.lastName,
-                  textInputType: TextInputType.name,
-                  callback: (newValue, controller) {
-                    setState(() {
-                      controllers[1] = controller;
-                    });
-                  },
-                ),
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                ),
-                SaidTextField(
-                  placeholder: AppLocalizations.of(context).age,
-                  text: widget.authenticatedUser.age?.toString(),
-                  textInputType: TextInputType.name,
-                  callback: (newValue, controller) {
-                    setState(() {
-                      controllers[2] = controller;
-                    });
-                  },
-                ),
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                ),
-                SaidDropdown(
-                  placeholder: AppLocalizations.of(context).sex,
-                  value: widget.authenticatedUser.sex,
-                  options: sexOptions,
-                  callback: (newValue) {
-                    setState(() {
-                      _sexValue = newValue!;
-                    });
-                  },
-                ),
-                const Padding(
-                  padding: EdgeInsets.all(16.0),
-                ),
-                SaidPrimaryButton(
-                    text: AppLocalizations.of(context).save,
-                    context: context,
-                    onPressed: _saveChangesAsync)
-              ],
-            )),
-      ],
-    )));
+        const SizedBox(
+          height: 32,
+        ),
+        SaidTextField(
+          placeholder: AppLocalizations
+              .of(context)
+              .firstName,
+          text: widget.authenticatedUser.firstName,
+          textInputType: TextInputType.name,
+          callback: (newValue, controller) {
+            setState(() {
+              controllers[0] = controller;
+            });
+          },
+        ),
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+        ),
+        SaidTextField(
+          placeholder: AppLocalizations
+              .of(context)
+              .lastName,
+          text: widget.authenticatedUser.lastName,
+          textInputType: TextInputType.name,
+          callback: (newValue, controller) {
+            setState(() {
+              controllers[1] = controller;
+            });
+          },
+        ),
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+        ),
+        SaidTextField(
+          placeholder: AppLocalizations
+              .of(context)
+              .age,
+          text: widget.authenticatedUser.age?.toString(),
+          textInputType: TextInputType.name,
+          callback: (newValue, controller) {
+            setState(() {
+              controllers[2] = controller;
+            });
+          },
+        ),
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+        ),
+        SaidDropdown(
+          placeholder: AppLocalizations
+              .of(context)
+              .sex,
+          value: widget.authenticatedUser.sex,
+          options: sexOptions,
+          callback: (newValue) {
+            setState(() {
+              _sexValue = newValue!;
+            });
+          },
+        ),
+        const Padding(
+          padding: EdgeInsets.all(16.0),
+        ),
+        SaidPrimaryButton(
+            text: AppLocalizations
+                .of(context)
+                .save,
+            context: context,
+            onPressed: _saveChangesAsync),
+        ],
+      ),
+    ),)
+    );
   }
 }
