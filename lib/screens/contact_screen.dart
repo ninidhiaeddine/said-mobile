bfa18885b9366f8ce7619ef46a866fc8ea62507e
Dhia Eddine Nini <dxn00@mail.aub.edu>
2023-03-21 21:48:44 +0200

    Made the Contact Screen expect the authenticatedUser data as a parameter instead of fetching it from scratch

--- lib/screens/contact_screen.dart
+++ lib/screens/contact_screen.dart
@@ -6,16 +6,17 @@ import 'package:said/config/color_constants.dart';
 import 'package:said/services/message_service.dart';
 import 'package:said/services/models/message.dart';
 import 'package:said/services/models/user.dart';
-import 'package:said/services/user_service.dart';
 import 'package:said/theme/text_styles.dart';
-import 'package:said/utils/said_session_manager.dart';
 import 'package:said/widgets/buttons/said_primary_button.dart';
 import 'package:said/widgets/buttons/said_icon_back_button.dart';
 import 'package:said/widgets/misc/said_social_media_bar.dart';
 import 'package:said/widgets/textfields//said_text_field.dart';
 
 class ContactScreen extends StatefulWidget {
-  const ContactScreen({Key? key}) : super(key: key);
+  const ContactScreen({Key? key, required this.authenticatedUser})
+      : super(key: key);
+
+  final User authenticatedUser;
 
   @override
   State<ContactScreen> createState() => _ContactScreenState();
@@ -25,21 +26,12 @@ class _ContactScreenState extends State<ContactScreen> {
   late String _messageValue = "";
   late TextEditingController _controller;
 
-  User? _user;
-
-  Future<void> _loadUserAsync() async {
-    // get user id:
-    int userId = await SaidSessionManager.getSessionValue('id');
-    print("Loading User Async | UserId = " + userId.toString());
-
-    // get user from API service:
-    _user = await UserService.getUser(userId);
-  }
-
   Future<void> _sendMessage(BuildContext context) async {
     // prepare message:
-    var message =
-        Message(user: _user!, message: _messageValue, dateSent: DateTime.now());
+    var message = Message(
+        user: widget.authenticatedUser!,
+        message: _messageValue,
+        dateSent: DateTime.now());
 
     // make api call:
     var response = await MessageService.addMessage(message);
@@ -80,12 +72,6 @@ class _ContactScreenState extends State<ContactScreen> {
   }
 
   @override
-  void initState() {
-    super.initState();
-    _loadUserAsync();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -97,37 +83,42 @@ class _ContactScreenState extends State<ContactScreen> {
           centerTitle: true,
           leading: const SaidIconBackButton()),
       backgroundColor: ColorConstants.secondaryColor,
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
-        child: Expanded(
-          child: Padding(
-              padding: const EdgeInsets.all(32),
-              child: Column(
-                children: [
-                  SaidTextField(
-                    placeholder: AppLocalizations.of(context).message,
-                    linesCount: 10,
-                    blackVariant: true,
-                    callback: (newValue, controller) => {
-                      setState(() {
-                        _messageValue = newValue;
-                        _controller = controller;
-                      })
-                    },
-                  ),
-                  const Padding(padding: EdgeInsets.all(4)),
-                  SaidPrimaryButton(
-                    text: AppLocalizations.of(context).send,
-                    context: context,
-                    icon: const Icon(Icons.send),
-                    enabled: _user != null,
-                    onPressed: () => _sendMessage(context),
-                  ),
-                  const Padding(padding: EdgeInsets.all(8)),
-                  Padding(
-                      padding: const EdgeInsets.all(60),
-                      child: Column(
+        child: Flex(
+          direction: Axis.vertical,
+          children: [
+            Expanded(
+              child: Padding(
+                  padding: const EdgeInsets.all(32),
+                  child: Column(
+                    children: [
+                      SaidTextField(
+                        placeholder: AppLocalizations.of(context).message,
+                        linesCount: 10,
+                        blackVariant: true,
+                        callback: (newValue, controller) => {
+                          setState(() {
+                            _messageValue = newValue;
+                            _controller = controller;
+                          })
+                        },
+                      ),
+                      const Padding(padding: EdgeInsets.all(4)),
+                      SaidPrimaryButton(
+                        text: AppLocalizations.of(context).send,
+                        context: context,
+                        icon: const Icon(Icons.send),
+                        enabled: widget.authenticatedUser != null,
+                        onPressed: () => _sendMessage(context),
+                      ),
+                      const SizedBox(
+                        height: 64,
+                      ),
+                      Column(
                         children: [
                           Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               const Icon(Icons.location_pin,
                                   color: Colors.white),
@@ -136,11 +127,12 @@ class _ContactScreenState extends State<ContactScreen> {
                                   style: const TextStyle(
                                       fontSize: 18,
                                       fontWeight: FontWeight.bold,
-                                      color: Colors.white))
+                                      color: Colors.white)),
                             ],
                           ),
                           const Padding(padding: EdgeInsets.all(4)),
                           Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               const Icon(Icons.phone, color: Colors.white),
                               const Padding(padding: EdgeInsets.all(6)),
@@ -153,6 +145,7 @@ class _ContactScreenState extends State<ContactScreen> {
                           ),
                           const Padding(padding: EdgeInsets.all(4)),
                           Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               const Icon(Icons.email_rounded,
                                   color: Colors.white),
@@ -165,26 +158,28 @@ class _ContactScreenState extends State<ContactScreen> {
                             ],
                           ),
                         ],
-                      )),
-                  const Padding(padding: EdgeInsets.all(2.0)),
-                  const Spacer(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(AppLocalizations.of(context).followUs,
-                          style: const TextStyle(
-                              fontSize: 20, color: Colors.white)),
-                      const Padding(padding: EdgeInsets.all(2)),
-                      Text(AppLocalizations.of(context).socialMedia,
-                          style: const TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: ColorConstants.primaryColor)),
+                      ),
+                      const Padding(padding: EdgeInsets.all(2.0)),
+                      const Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(AppLocalizations.of(context).followUs,
+                              style: const TextStyle(
+                                  fontSize: 20, color: Colors.white)),
+                          const Padding(padding: EdgeInsets.all(2)),
+                          Text(AppLocalizations.of(context).socialMedia,
+                              style: const TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: ColorConstants.primaryColor)),
+                        ],
+                      ),
+                      SaidSocialMediaBar(),
                     ],
-                  ),
-                  SaidSocialMediaBar(),
-                ],
-              )),
+                  )),
+            )
+          ],
         ),
       ),
     );
