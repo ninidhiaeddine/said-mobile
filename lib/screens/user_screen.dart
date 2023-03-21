19ec2cb391eda9175af708d2078d2debb3aba6a3
Dhia Eddine Nini <dxn00@mail.aub.edu>
2023-03-21 21:50:09 +0200

    Massive overhaul to the main user screen, better initialization of values

--- lib/screens/user_screen.dart
+++ lib/screens/user_screen.dart
@@ -5,6 +5,7 @@ import 'package:said/fragments/club_fragment.dart';
 import 'package:said/fragments/home_fragment.dart';
 import 'package:said/fragments/info_fragment.dart';
 import 'package:said/fragments/announcements_fragment.dart';
+import 'package:said/screens/refresh_user_interface.dart';
 import 'package:said/services/models/user.dart';
 import 'package:said/utils/said_session_manager.dart';
 import 'package:said/widgets/nav/said_bottom_nav_bar.dart';
@@ -18,10 +19,21 @@ class UserScreen extends StatefulWidget {
   State<StatefulWidget> createState() => _UserScreenState();
 }
 
-class _UserScreenState extends State<UserScreen> {
+class _UserScreenState extends State<UserScreen>
+    implements RefreshUserInterface {
   int _selectedIndex = 2;
+  late Future<void> initAsync;
 
   late User _authenticatedUser;
+  late List<Icon> icons = const [
+    Icon(Icons.groups_rounded),
+    Icon(Icons.info_rounded),
+    Icon(Icons.home_rounded),
+    Icon(Icons.notifications_rounded),
+    Icon(Icons.account_circle_rounded),
+  ];
+  late List<String> labels;
+  late List<Widget> fragments;
 
   void _onItemTapped(int index) {
     setState(() {
@@ -29,61 +41,103 @@ class _UserScreenState extends State<UserScreen> {
     });
   }
 
-  Future<void> _loadUserAsync() async {
+  Future<User> _loadUserAsync() async {
     var user = await SaidSessionManager.getUser();
     setState(() {
       _authenticatedUser = user;
     });
+
+    return user;
+  }
+
+  Future<void> init() async {
+    // wait on loading the user:
+    User user = await _loadUserAsync();
+
+    // prepare views:
+    fragments = [
+      const ClubFragment(),
+      InfoFragment(authenticatedUser: user),
+      HomeFragment(authenticatedUser: user),
+      const AnnouncementsFragment(),
+      AccountFragment(
+        authenticatedUser: user,
+        onRefreshUser: onRefreshUser,
+      )
+    ];
   }
 
   @override
   void initState() {
     super.initState();
-    _loadUserAsync();
+    initAsync = init();
+    initAsync;
   }
 
   @override
-  Widget build(BuildContext context) {
-    // prepare icons:
-    List<Icon> icons = const [
-      Icon(Icons.groups_rounded),
-      Icon(Icons.info_rounded),
-      Icon(Icons.home_rounded),
-      Icon(Icons.notifications_rounded),
-      Icon(Icons.account_circle_rounded),
-    ];
+  void didChangeDependencies() {
+    super.didChangeDependencies();
 
-    // prepare labels:
-    List<String> labels = [
+    // init strings:
+    labels = [
       AppLocalizations.of(context).club,
       AppLocalizations.of(context).info,
       AppLocalizations.of(context).home,
       AppLocalizations.of(context).announcements,
       AppLocalizations.of(context).account,
     ];
+  }
 
-    // prepare views:
-    List<Widget> views = [
-      const ClubFragment(),
-      InfoFragment(authenticatedUser: _authenticatedUser),
-      HomeFragment(authenticatedUser: _authenticatedUser),
-      const AnnouncementsFragment(),
-      AccountFragment(authenticatedUser: _authenticatedUser)
-    ];
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: initAsync,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Scaffold(
+              body: Center(
+                child: Text(AppLocalizations.of(context).loading),
+              ),
+            );
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            return Scaffold(
+                appBar: AppBar(
+                  backgroundColor: ColorConstants.backgroundColor,
+                  elevation: 0,
+                ),
+                drawer: const SaidNavigationDrawer(),
+                bottomNavigationBar: SaidBottomNavigationBar(
+                  icons: icons,
+                  labels: labels,
+                  selectedIndex: _selectedIndex,
+                  onTap: _onItemTapped,
+                ),
+                body: fragments[_selectedIndex]);
+          } else {
+            return Scaffold(
+                body: Center(
+              child: Text(AppLocalizations.of(context).unexpectedError),
+            ));
+          }
+        });
+  }
 
-    // return scaffold with bottom navigation bar and corresponding view
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: ColorConstants.backgroundColor,
-          elevation: 0,
-        ),
-        drawer: const SaidNavigationDrawer(),
-        bottomNavigationBar: SaidBottomNavigationBar(
-          icons: icons,
-          labels: labels,
-          selectedIndex: _selectedIndex,
-          onTap: _onItemTapped,
-        ),
-        body: views[_selectedIndex]);
+  Future<void> _saveUserToSessionAsync(User updatedUser) async {
+    SaidSessionManager.storeUser(
+        id: updatedUser.id!,
+        username: updatedUser.username,
+        email: updatedUser.email,
+        firstName: updatedUser.firstName,
+        lastName: updatedUser.lastName,
+        sex: updatedUser.sex,
+        age: updatedUser.age);
+  }
+
+  @override
+  void onRefreshUser(User updatedUser) {
+    _saveUserToSessionAsync(updatedUser);
+    setState(() {
+      _authenticatedUser = updatedUser;
+    });
   }
 }
